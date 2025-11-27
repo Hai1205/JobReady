eb6ad2e7234cd042055c0a04c21fdc039b154d72
Nguyen Hoang Hai <hainguyenhoang1205@gmail.com>
2025-11-27 20:51:25 +0700

    add debug

--- client/services/constants.ts
+++ client/services/constants.ts
@@ -1,2 +1,5 @@
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL!;
 export const NODE_ENV = process.env.NEXT_PUBLIC_NODE_ENV!;
+
+console.log('🔍 SERVER_URL:', SERVER_URL);
+console.log('🔍 NEXT_PUBLIC_SERVER_URL:', process.env.NEXT_PUBLIC_SERVER_URL);
